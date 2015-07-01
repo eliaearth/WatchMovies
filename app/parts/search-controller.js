d684d1114ee4e151a8049e0112491fd271d5ae29
elia <elia_earth@yahoo.com>
2015-07-01 13:40:19 +0300

    add search rest call, populate the search model

--- app/parts/search-controller.js
+++ app/parts/search-controller.js
@@ -1,14 +1,22 @@
 (function(){
     var mainApp = angular.module('mainApp');
 
-    mainApp.controller('searchController', ['$scope', '$state', 'searchService', 'searchModel',
-        function($scope, $state, searchService, searchModel){
+    mainApp.controller('searchController', ['$scope', '$log', '$state', 'searchService', 'searchModel',
+        function($scope, $log, $state, searchService, searchModel){
             $scope.searchModel = searchModel;                                
             console.log('searchController init');
             //$scope.state = $state;
+            $scope.onSearchSuccess = function(response){
+                searchModel.moviesFound = response;
+                $log.error('Search success');
+            };
+            $scope.onSearchError = function(){
+               $log.error('Search error');
+            };
             $scope.onSearch = function(){
                 console.log('search text model = ' + $scope.searchModel.textTofind);
-                searchService.movieTitle = searchTxt.value;
+                searchModel.textTofind = searchTxt.value;
+                searchService.getMovies(searchTxt.value).success(onSearchSuccess()).error(onSearchError());
                 $state.go('results');
             };
         }
--- app/parts/search-service.js
+++ app/parts/search-service.js
@@ -1,6 +1,9 @@
 (function(){
     var mainApp = angular.module('mainApp');
     mainApp.service('searchService', ['$http', function($http){
-            this.movieTitle = "";
+        var getMovies = function(movieTitle){
+             return $http({method: 'GET', url: 'http://178.62.41.61/TVMateServer/app/getShow/name=' + movieTitle});
+         } 
+        return getMovies;
     }]);
 }())
